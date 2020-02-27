78c2754f4be06970aeb440a9887cee6ab791f8d1
genar335 <domvlielupe3@gmail.com>
2020-02-27 01:42:22 +0000

    First commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+
+            <h1>About</h1>
+            <p>
+                 This is the TodoList, my first, definetly original, react project!
+            </p>
+        </React.Fragment>
+    )
+}
+export default About;
